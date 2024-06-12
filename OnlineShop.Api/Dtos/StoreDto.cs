1e45c94ec27978cb17a8efcb93dbe6b1d67e862d
Renutte <danizimerga@gmail.com>
2024-06-12 02:22:04 +0200

    remove all

--- OnlineShop.Api/Dtos/StoreDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineShop.Api.Dtos
-{
-    public class StoreDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Location { get; set; }
-    }
-}
